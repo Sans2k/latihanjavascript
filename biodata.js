bdb338618f1a22bef894710bd384ad0178b85eb9
Sans2k <140954881+Sans2k@users.noreply.github.com>
2023-10-03 11:08:30 +0700

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,11 @@
+let nama = "sansan nur azis";
+let kelas = "XI-PPLG-3";
+let tempatTanggallahir = "Tasikmalaya,17-05-2006";
+let alamat = "kampung pelang"
+
+document.getElementById("biodata").innerHTML =
+"BIODATA"+
+"<br>Nama : "+ nama +
+"<br>Kelas : "+ kelas +
+"<br>Tempat, Tanggal lahir : "+ tempatTanggallahir +
+"<br>Alamat : "+ alamat ;
